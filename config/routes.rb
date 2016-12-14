4d2040da2e03eac7b69d0fa463de34bffcb77089
sameer <sameer.shaik@studentpartner.com>
2016-12-14 20:40:37 +0530

    create static page1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'jobs/index'
+
   get 'pages/index'
 
   resources :jobs
--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
-class JobsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class JobsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get jobs_index_url
+    assert_response :success
+  end
+
 end
